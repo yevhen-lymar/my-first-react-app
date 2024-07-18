3265d9a00ac20d92017906f0e0fd26be6d56c6f2
Yevhen <work.lymar@gmail.com>
2024-07-18 16:58:39 +0300

    third commit in work. Date 18-07-24|16-58

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import React, { useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 
+function MyButton() {
+  return (
+    <button>Click for ME</button>
+  );
+}
+
+function AboutPage(){
+  return (
+    <>
+    <h1>About</h1>
+    <p>Hello there.<br /> How do you do?</p>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <AboutPage />
+        <MyButton />
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
